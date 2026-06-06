aaf0eed29e27e7f4603ccf4160c60f35a9aa64ca
IVAN <126900030+PahanIvan2007@users.noreply.github.com>
2026-06-07 01:21:45 +0300

    Fix: add credentials.yml.enc + master.key for secret_key_base

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,7 +5,5 @@ bundle install
 yarn install
 yarn build:css
 
-# Generate temporary secret for asset compilation (runtime uses SECRET_KEY_BASE from env)
-export SECRET_KEY_BASE=$(bundle exec rails secret)
 bundle exec rails assets:precompile
 bundle exec rails db:migrate

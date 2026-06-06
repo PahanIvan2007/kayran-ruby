4e8165e865d1d113865c0610fc95177e01bfb876
IVAN <126900030+PahanIvan2007@users.noreply.github.com>
2026-06-07 01:17:28 +0300

    Fix: add SECRET_KEY_BASE fallback for build, set executable bit on render-build.sh

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -4,5 +4,8 @@ set -o errexit
 bundle install
 yarn install
 yarn build:css
+
+# Generate temporary secret for asset compilation (runtime uses SECRET_KEY_BASE from env)
+export SECRET_KEY_BASE=$(bundle exec rails secret)
 bundle exec rails assets:precompile
 bundle exec rails db:migrate
